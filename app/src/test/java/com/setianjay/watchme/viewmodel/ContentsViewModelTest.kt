2d874c336150f133f6873a1830e26450d0c0b167
Hari Setiaji <hari.setiaji2000@gmail.com>
2022-01-02 16:29:17 +0700

    Fix code in contents unit test based on code reviewer's suggestion

--- app/src/test/java/com/setianjay/watchme/viewmodel/ContentsViewModelTest.kt
+++ app/src/test/java/com/setianjay/watchme/viewmodel/ContentsViewModelTest.kt
@@ -3,84 +3,144 @@ package com.setianjay.watchme.viewmodel
 import com.setianjay.watchme.utils.DataDummyUtil
 import junit.framework.Assert.*
 import org.junit.Before
+import org.junit.FixMethodOrder
 import org.junit.Test
+import org.junit.runners.MethodSorters
 
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
 class ContentsViewModelTest {
 
     private lateinit var viewModel: ContentsViewModel
 
+    private val dummyMovies get() = DataDummyUtil.generateDataMovies()
+    private val dummyTvShows get() = DataDummyUtil.generateDataTvShows()
+
     @Before
-    fun setup(){
+    fun setup() {
         viewModel = ContentsViewModel()
     }
 
     /**
-     * testing to get data movies
+     * testing to get movies data size, equal with movies resources data size
      *
-     * note: for obtain data movies, the arguments isMovies must true
+     * note: for obtain movies data, the arguments isMovies must true
      * */
     @Test
-    fun testGetDataMovies() {
-        val movies = viewModel.getDataMovies(true)
-        assertNotNull(movies)
-        assertEquals(DataDummyUtil.generateDataMovies().size, movies.size)
+    fun testGetDataMoviesSize() {
+        checkMoviesDataSize(isMovies = true)
     }
 
 
     /**
-     * testing to get data tv shows
+     * testing to get tv shows data size, equal with tv shows resources data size
      *
-     * note: for obtain data tv shows, the arguments isMovies must false
+     * note: for obtain tv shows data, the arguments isMovies must false
      * */
     @Test
-    fun testGetDataTvShows() {
-        val tvShows = viewModel.getDataMovies(false)
-        assertNotNull(tvShows)
-        assertEquals(DataDummyUtil.generateDataTvShows().size, tvShows.size)
+    fun testGetDataTvShowsSize() {
+        checkMoviesDataSize(isMovies = false)
     }
 
 
     /**
-     * testing to get specific data movies
+     * testing to get specific movies, equal with movies resources data based on position
      *
      * note: for obtain specific data of movies, the arguments isMovies must true
      * */
-    //success
     @Test
-    fun testGetSpecificDataMovies() {
-        val getSpecificDataMovies = viewModel.getSpecificDataMovies(0, true)
-        assertNotNull(getSpecificDataMovies)
-        assertEquals(DataDummyUtil.generateDataMovies()[0].title, getSpecificDataMovies.title)
-    }
-
-    //fail, because the title not equals
-    @Test
-    fun tesGetSpecificDataMoviesFail(){
-        val movies = viewModel.getSpecificDataMovies(0, false)
-        assertNotNull(movies)
-        assertEquals(DataDummyUtil.generateDataMovies()[0].title, movies.title)
+    fun testGetSpecificMoviesData() {
+        checkSpecificMovie(isMovies = true, positionAt = 0)
     }
 
     /**
-     * testing to get specific tv shows
+     * testing to get specific tv shows, equal with tv shows resources data based on position
      *
      * note: for obtain specific data of tv shows, the arguments isMovies must false
      * */
-    //success
     @Test
-    fun testGetSpecificTvShows() {
-        val getSpecificTvShows = viewModel.getSpecificDataMovies(0, false)
-        assertNotNull(getSpecificTvShows)
-        assertEquals(DataDummyUtil.generateDataTvShows()[0].title, getSpecificTvShows.title)
+    fun testGetSpecificTvShowsData() {
+        checkSpecificMovie(isMovies = false, positionAt = 1)
     }
 
-    //fail, because the title not equals
+    /**
+     * testing to get specific movies, with wrong passing argument based on position
+     *
+     * note: for obtain specific data of movies, the arguments isMovies must true.
+     *       in this function we pass the wrong arguments isMovies, namely false.
+     * */
     @Test
-    fun tesGetSpecificTvShowsFail(){
-        val movies = viewModel.getSpecificDataMovies(0, true)
-        assertNotNull(movies)
-        assertEquals(DataDummyUtil.generateDataTvShows()[0].title, movies.title)
+    fun testWrongGetSpecificMoviesData(){
+        checkSpecificWrongMovie(isMovies = false, positionAt = 0)
     }
 
+    /**
+     * testing to get specific tv shows, with wrong passing argument based on position
+     *
+     * note: for obtain specific data of tv shows, the arguments isMovies must false.
+     *       in this function we pass the wrong arguments isMovies, namely true.
+     * */
+    @Test
+    fun testWrongGetSpecificTvShowsData(){
+        checkSpecificWrongMovie(isMovies = true, positionAt = 1)
+    }
 
+    /**
+     * check size of movies data
+     *
+     * @param isMovies      <code>true</code> obtain list of movie data, otherwise list of tv show data (@field movie)
+     *
+     * @output              success, if size of list data is same with data resources size
+     * */
+    private fun checkMoviesDataSize(isMovies: Boolean){
+        val movie = viewModel.getDataMovies(isMovies)
+        val moviesResourcesData = if (isMovies) dummyMovies else dummyTvShows
+        assertNotNull(movie)
+        assertEquals(moviesResourcesData.size, movie.size)
+    }
+
+    /**
+     * check specific movie, whether is same
+     *
+     * @param isMovies      <code>true</code> obtain movie data, otherwise tv show data (@field movie)
+     * @param positionAt    the position of the data you want to check
+     *
+     * @output              success, if data is same with data resources
+     * */
+    private fun checkSpecificMovie(isMovies: Boolean, positionAt: Int) {
+        val movie = viewModel.getSpecificDataMovies(positionAt, isMovies)
+        //get data resource based on argument
+        val moviesResourceData =
+            if (isMovies) dummyMovies else dummyTvShows
+        assertNotNull(movie)
+        assertEquals(moviesResourceData[positionAt].title, movie.title)
+        assertEquals(moviesResourceData[positionAt].poster, movie.poster)
+        assertEquals(moviesResourceData[positionAt].duration, movie.duration)
+        assertEquals(moviesResourceData[positionAt].director, movie.director)
+        assertEquals(moviesResourceData[positionAt].rating, movie.rating)
+        assertEquals(moviesResourceData[positionAt].genre.size, movie.genre.size)
+        assertEquals(moviesResourceData[positionAt].overview, movie.overview)
+    }
+
+    /**
+     * check specific movie, whether is wrong
+     *
+     * @param isMovies      <code>true</code> obtain movie data, otherwise tv show data (@field movie)
+     * @param positionAt    the position of the data you want to check
+     *
+     * @output              success, if data is not same with data resources
+     * */
+    private fun checkSpecificWrongMovie(isMovies: Boolean, positionAt: Int){
+        val movie = viewModel.getSpecificDataMovies(positionAt, isMovies)
+        //get data resource based on argument
+        val moviesResourceData =
+            if (isMovies) dummyTvShows else dummyMovies
+        assertNotNull(movie)
+        assertNotSame(moviesResourceData[positionAt].title, movie.title)
+        assertNotSame(moviesResourceData[positionAt].poster, movie.poster)
+        assertNotSame(moviesResourceData[positionAt].duration, movie.duration)
+        assertNotSame(moviesResourceData[positionAt].director, movie.director)
+        assertNotSame(moviesResourceData[positionAt].rating, movie.rating)
+        assertNotSame(moviesResourceData[positionAt].genre.size, movie.genre.size)
+        assertNotSame(moviesResourceData[positionAt].overview, movie.overview)
+    }
 }
