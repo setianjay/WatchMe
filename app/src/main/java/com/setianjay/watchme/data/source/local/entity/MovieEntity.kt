1992ff58c5fbd9d3b468522b04a693a982f0086a
Hari Setiaji <hari.setiaji2000@gmail.com>
2022-02-01 17:49:56 +0700

    Configure entity, dao, and database for local persistence

--- app/src/main/java/com/setianjay/watchme/data/source/local/entity/MovieEntity.kt
+++ app/src/main/java/com/setianjay/watchme/data/source/local/entity/MovieEntity.kt
@@ -1,11 +1,41 @@
 package com.setianjay.watchme.data.source.local.entity
 
+import android.os.Parcelable
+import androidx.annotation.NonNull
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.parcelize.Parcelize
+
+@Entity(tableName = "tbl_movie")
+@Parcelize
 data class MovieEntity(
+    @PrimaryKey
+    @NonNull
+    @ColumnInfo(name = "movie_id")
     val movieId: Long,
+
+    @ColumnInfo(name = "title")
     val title: String,
+
+    @ColumnInfo(name = "poster")
     val poster: String,
+
+    @ColumnInfo(name = "overview")
     val overview: String,
+
+    @ColumnInfo(name = "rating")
     val rating: Float,
+
+    @ColumnInfo(name = "release")
     val release: String,
-    val genre: String
-)
+
+    @ColumnInfo(name = "genre")
+    val genre: String,
+
+    @ColumnInfo(name = "is_movie")
+    val isMovies: Boolean,
+
+    @ColumnInfo(name = "is_favorite")
+    var isFavorite: Boolean = false
+): Parcelable
